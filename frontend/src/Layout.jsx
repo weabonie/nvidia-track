864930bc41076ed666c1533c3cd92ed921d9cd70
Duc Nguyen <howtodrawapicture@gmail.com>
2025-11-08 17:36:51 -0600

    Added shadcn ui

--- frontend/src/Layout.jsx
+++ frontend/src/Layout.jsx
@@ -4,7 +4,7 @@ import { Outlet } from 'react-router-dom';
 
 const Layout = ({children}) => {
     return (
-        <div className="flex flex-col min-h-screen bg-black">
+        <div className="flex flex-col min-h-svh w-full bg-black">
             <Navbar />
             <main className="flex-grow">
                 <Outlet/>
---
+++ frontend/src/pages/LoginPage.jsx
@@ -0,0 +1,9 @@
+const Login = () => {
+    return (
+        <div className="w-full max-w-sm">
+            <div>
+                
+            </div>
+        </div>
+    )
+}

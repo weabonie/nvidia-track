297fbf30c04fdadc5a8fea8b244ba079c514171a
Waz9pluz10 <36023438+SiruX21@users.noreply.github.com>
2025-11-08 19:19:15 -0600

    Login Page Animation Added

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -2,9 +2,9 @@ import LoginForm from "../components/LoginForm";
 
 const LoginPage = () => {
   return (
-    <div className="w-full h-full">
+    <div className="w-full h-full animate-fade-in">
       <div className="bg-gradient-to-b from-lime-900 to-zinc-900 to-lime-800 w-1/2 min-h-svh bg-black flex flex-col justify-center items-center">
-        <div>
+        <div className="animate-scale-in opacity-0" style={{ animationDelay: "0.3s" }}>
             <LoginForm/>
         </div>
       </div>
